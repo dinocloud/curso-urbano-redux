0e54cb5f6918a9fdff8cd9d3a6785d4a874313e7
Luciano Alzugaray <luciano.alzugaray@dinocloudconsulting.com>
2020-11-24 00:57:36 -0300

    Agregado master

--- src/store/counter/counter.actions.tsx
+++
@@ -1,9 +0,0 @@
-import store from '../store'
-
-export const increment = () => {
-    store.dispatch({ type: 'INCREMENT' })
-}
-
-export const decrement = () => {
-    store.dispatch({ type: 'DECREMENT' })
-}
